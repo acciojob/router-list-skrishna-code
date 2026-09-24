1d9828bec110452fe54fb03d80fb85598cd5fba5
skrishna-code <krishnasutar026@gmail.com>
2026-09-24 09:48:40 +0530

    Implement routing for ItemList and ItemDetail

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
 import React from "react";
-import './../styles/App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
-const App = () => {
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
+
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ItemList />} />
+        <Route path="/items/:id" element={<ItemDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
