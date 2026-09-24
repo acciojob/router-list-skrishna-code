17647dd5281e37d13c6cea2d42c3c465db8de085
skrishna-code <krishnasutar026@gmail.com>
2026-09-24 09:49:09 +0530

    Add ItemList component to display a list of items

---
+++ src/components/ItemList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ItemList() {
+  const items = [
+    {
+      id: 1,
+      name: "Item 1"
+    },
+    {
+      id: 2,
+      name: "Item 2"
+    },
+    {
+      id: 3,
+      name: "Item 3"
+    }
+  ];
+
+  return (
+    <div>
+      <h1>Item List</h1>
+
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <Link to={`/items/${item.id}`}>
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ItemList;
